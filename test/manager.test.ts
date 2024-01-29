7cec2c589d68e994f61afdaa85fe38a97e62f6f6
JacobLinCool <jacoblincool@gmail.com>
2024-01-30 02:25:32 +0800

    test: change download model from tiny to base

--- test/manager.test.ts
+++ test/manager.test.ts
@@ -6,20 +6,20 @@ import path from "node:path";
 it(
 	"model manager should work",
 	async () => {
-		const p = path.join(manager.dir.models, "tiny.bin");
+		const p = path.join(manager.dir.models, "base.bin");
 		if (fs.existsSync(p)) {
 			fs.unlinkSync(p);
 		}
 
-		expect(manager.list()).not.toContain("tiny");
+		expect(manager.list()).not.toContain("base");
 
-		const name = await manager.download("tiny");
-		expect(name).toBe("tiny");
+		const name = await manager.download("base");
+		expect(name).toBe("base");
 
 		const resolved = manager.resolve(name);
 		expect(fs.existsSync(resolved)).toBe(true);
 
-		expect(manager.list()).toContain("tiny");
+		expect(manager.list()).toContain("base");
 	},
-	{ timeout: 60_000 },
+	{ timeout: 120_000 },
 );
